40c02bc0ff06f8035c0b927c6f729ed0dc02e281
Alex Fletcher <AlexFletcher@users.noreply.github.com>
2023-07-02 20:58:07 +0100

    Update AbstractChemistry.hpp

--- src/AbstractChemistry.hpp
+++ src/AbstractChemistry.hpp
@@ -18,53 +18,146 @@
  */
 class AbstractChemistry 
 {
-private:
+protected:
+
+    /**
+     * Number of chemicals.
+     * Initialised to zero in constructor.
+     */
     unsigned mNumberChemicals;
 
-protected:
-    std::vector<AbstractChemical*> mpChemicalVector;
+    /**
+     * Vector of pointers to chemicals. 
+     * Initialised to empty vector in constructor.
+     */
+    std::vector<AbstractChemical*> mChemicalVector;
 
+    /**
+     * Vector of names of chemicals. 
+     * Initialised to empty vector in constructor.
+     */
     std::vector<std::string> mChemicalNames;
 
+    /**
+     * Vector of chemical dimensions. 
+     * Initialised to empty vector in constructor.
+     */
     std::vector<std::string> mChemicalDimensions;
     
 public:
+
+    /**
+     * Constructor.
+     */
     AbstractChemistry();
 
-    virtual ~AbstractChemistry()
-    {
-    };
+    /**
+     * Destructor.
+     */
+    virtual ~AbstractChemistry();
 
-    virtual void AddChemical(AbstractChemical*);
+    /**
+     * Check whether a given chemical is already present, and if it isn't, 
+     * add it to mChemicalVector.
+     *
+     * @param pChemical pointer to a chemical
+     */
+    virtual void AddChemical(AbstractChemical* pChemical);
 
-    virtual bool CheckChemical(AbstractChemical*);
+    /**
+     * @param pChemical pointer to a chemical
+     *
+     * @return whether pChemical is present.
+     */
+    virtual bool CheckChemical(AbstractChemical* pChemical);
 
-    virtual void UpdateChemicalVectors(AbstractChemical*);
+    /**
+     * \todo document method
+     *
+     * @param pChemical pointer to a chemical
+     */
+    virtual void UpdateChemicalVectors(AbstractChemical* pChemical);
 
-    void SetChemicalVector(std::vector<AbstractChemical*>);
+    /**
+     * \todo document method
+     *
+     * @param chemicalVector vector of pointers to chemicals
+     */
+    void SetChemicalVector(std::vector<AbstractChemical*> chemicalVector);
 
-    void SetChemicalNames(std::vector<std::string>);
+    /**
+     * \todo document method
+     *
+     * @param chemicalNames vector of names of chemicals
+     */
+    void SetChemicalNames(std::vector<std::string> chemicalNames);
 
-    void SetChemicalDimensions(std::vector<std::string>);
+    /**
+     * \todo document method
+     *
+     * @param chemicalDimensions vector of chemical dimensions
+     */
+    void SetChemicalDimensions(std::vector<std::string> chemicalDimensions);
 
+    /**
+     * @return mChemicalDimensions
+     */
     std::vector<AbstractChemical*> rGetChemicalVector();
 
+    /**
+     * @return mChemicalNames
+     */
     std::vector<std::string> GetChemicalNames();
 
-    std::string GetChemicalNamesByIndex(unsigned);
+    /**
+     * \todo throw exception if no chemical has this index
+     *
+     * @param index \todo document argument
+     *
+     * @return \todo document return type
+     */
+    std::string GetChemicalNamesByIndex(unsigned index);
 
-    unsigned GetChemicalIndexByName(std::string);
+    /**
+     * \todo throw exception if no chemical has this name
+     *
+     * @param name name of a chemical
+     *
+     * @return pointer to the chemical with this name
+     */
+    unsigned GetChemicalIndexByName(std::string name);
 
+    /**
+     * @return mChemicalDimensions
+     */
     std::vector<std::string> GetChemicalDimensions();
+    /**
+     * \todo throw exception if no chemical has this index
+     *
+     * @param index \todo document argument
+     *
+     * @return \todo document return type
+     */
+    std::string GetChemicalDimensionsByIndex(unsigned index);
 
-    std::string GetChemicalDimensionsByIndex(unsigned);
-
+    /**
+     * \todo document method
+     *
+     * @return \todo document return type
+     */
     virtual std::string GetChemistryType();
 
-    virtual void AddChemistry(AbstractChemistry*);
+    /**
+     * \todo document method
+     *
+     * @param pChemistry pointer to a chemistry
+     */
+    virtual void AddChemistry(AbstractChemistry* pChemistry);
 
+    /**
+     * @return mNumberChemicals
+     */
     unsigned GetNumberChemicals();
-
 };
 
-#endif
+#endif /* ABSTRACTCHEMISTRY_HPP_ */
